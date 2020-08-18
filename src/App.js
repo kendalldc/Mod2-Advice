bb592e9a967adad5a3d052c3f12b80cd10f5571a
kendalldc <kdendtler@gmail.com>
2020-08-18 19:21:49 -0400

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav'
+import Home from './Components/Home'
+import Life from './Components/Life'
+import Love from './Components/Love'
+import Cover from './Components/Cover'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+  return(
+    <Router>
+<div className="App">
+<Nav />
+<Switch>
+<Route exact path='/' component={Home} />
+<Route path='/life' component={Life} />
+<Route path='/love' component={Love} />
+<Route path='/cover' component={Cover} />
+</Switch>
+</div>
+</Router>
+  )
 }
+
+
+
+
+
 
 export default App;
