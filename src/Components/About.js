a4421fd6ed9627f28eac1a1dad612a11b23a2989
kendalldc <67480485+kendalldc@users.noreply.github.com>
2020-08-19 12:31:49 -0400

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,11 +4,19 @@ import React from 'react'
 
 function About(){
     return(
-  
-  <div className="About">
-  
-  <h1>Everyone Needs Advice</h1>
-  </div>
+      <div className="About">
+      <div className="grid-container">
+      <div className="img">
+        <div className="title"><h1>Everyone Needs Help</h1></div>
+        <div className="pic"><img src="https://cdn2.psychologytoday.com/assets/center/2020-05/center-illustration-therapy.png" alt=""/></div>
+      </div>
+      <div className="text">
+      <p>Many of us have a narrow view of therapy. We think it’s solely for navigating clinical depression or severe anxiety or roller-coaster moods. We think it’s only an option when we’re going through a major crisis, a big transition, or a prolonged, persistent period of grief. We think therapy is only an option when relationships become disconnected, and marriages are on the brink of divorce.
+    
+    While therapy is important and vital for all the above, it’s also helpful for a lot of other reasons, and you don’t have to wait until the walls are falling down to work with a clinician. You can go when the paint is chipping—or when you’d like a different color on your walls.</p>
+      </div>
+    </div>
+    </div>
   
     )
   }
@@ -16,3 +24,4 @@ function About(){
 
 
 export default About
+
