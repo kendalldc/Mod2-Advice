08b705a188176ee125601cd6a94fd89d7a0e94cb
kendalldc <kdendtler@gmail.com>
2020-08-19 09:50:43 -0400

    button play

--- src/Components/Cover.js
+++ src/Components/Cover.js
@@ -8,7 +8,7 @@ function Cover (){
         <div className="cover">
 <div className="primary-nav"></div>
 <h1 className="cover-Text" > ADVISE ME.</h1>  <br/>     
-<Link to="/home"><button className="cover-Button">Enter</button></Link>
+{/* <Link to="/home"><button className="cover-Button">Enter</button></Link> */}
 {/* <Enter/> */}
  </div>
     )
