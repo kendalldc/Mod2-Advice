0f78ce0ad2642b1d3d711a4564366df4fc37f5f7
kendalldc <kdendtler@gmail.com>
2020-08-19 00:30:13 -0400

    grid use

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,9 +4,13 @@ import React from 'react';
 
 function Home (){
     return(
+        
 <div className="Home">
    <h1 className="home-Advise"> ADVISE ME.</h1>
    <h3 className="home-Vice">What's your Vice?</h3>
+   
+   
+   {/* row2 */}
    <button className="vice-buttons">Love</button> <button  className="vice-buttons">Life</button>
 </div>
         )
