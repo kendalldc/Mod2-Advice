a9817e893891f278409f5e499f568c97d6c5ea84
kendalldc <67480485+kendalldc@users.noreply.github.com>
2020-08-19 12:33:02 -0400

    Update Cover.js

--- src/Components/Cover.js
+++ src/Components/Cover.js
@@ -9,7 +9,7 @@ function Cover (){
 <div className="primary-nav"></div>
 <h1 className="cover-Text" > ADVISE ME.</h1>  <br/>     
 <Link to="/home"><button className="cover-Button">Enter</button></Link>
-{/* <Enter> */}
+{/* <Enter/> */}
  </div>
     )
 }
